1b44193dc5f43cf0f54ee0dc832466b81c43ff9e
de-gupta <de-gupta>
2025-08-15 10:30:02 +0200

    [maven-release-plugin] prepare release v0.0.2

--- src/main/java/de/gupta/validation/aegis/api/validation/Validations.java
+++ src/main/java/de/gupta/validation/aegis/api/validation/Validations.java
@@ -3,13 +3,21 @@ package de.gupta.validation.aegis.api.validation;
 import de.gupta.validation.aegis.api.specification.collection.CollectionSpecificationFactory;
 import de.gupta.validation.aegis.api.specification.comparison.ComparableSpecificationFactory;
 import de.gupta.validation.aegis.api.specification.comparison.ComparisonType;
+import de.gupta.validation.aegis.api.specification.object.ObjectSpecificationFactory;
 
 import java.util.Collection;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public final class ValidationSpecificationFactory
+public final class CustomValidationSpecifications
 {
+	public static <T, V extends RuntimeException> Validation<T> notNullSpecification(
+			final Function<T, ?> extractor,
+			final Supplier<V> exceptionSupplier)
+	{
+		return ValidationFactory.from(extractor, ObjectSpecificationFactory.notNull(), exceptionSupplier);
+	}
+
 	public static <T, W extends Comparable<W>, V extends RuntimeException> Validation<T> comparisonSpecification(
 			final Function<T, ? extends W> extractor, final Function<T, ? extends W> thresholdExtractor,
 			final ComparisonType comparisonType,
@@ -42,7 +50,7 @@ public final class ValidationSpecificationFactory
 		);
 	}
 
-	private ValidationSpecificationFactory()
+	private CustomValidationSpecifications()
 	{
 	}
 }
