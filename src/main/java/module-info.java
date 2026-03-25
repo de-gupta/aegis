17f0ce88f85419229199ed4277535bd13b345acd
de-gupta <de-gupta>
2026-03-25 16:35:39 +0100

    added module info for jpms support

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module de.gupta.validation.aegis
+{
+	requires de.gupta.aletheia;
+	requires de.gupta.athena;
+
+	exports de.gupta.validation.aegis.api.specification;
+	exports de.gupta.validation.aegis.api.specification.collection;
+	exports de.gupta.validation.aegis.api.specification.comparison;
+	exports de.gupta.validation.aegis.api.specification.number;
+	exports de.gupta.validation.aegis.api.specification.object;
+	exports de.gupta.validation.aegis.api.specification.string;
+	exports de.gupta.validation.aegis.api.specification.time;
+	exports de.gupta.validation.aegis.api.validation;
+	exports de.gupta.validation.aegis.api.validation.factories;
+	exports de.gupta.validation.aegis.api.validator;
+}
